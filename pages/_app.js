cc9d49a919dc6dddb16a49057ee1a1cc13c30738
AK <a.konken@parallelmade.com>
2022-03-10 22:58:32 -0500

    Update Netlify CMS identity widget to be loaded on pages;

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+import Script from "next/script";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default Application
+export default Application;
--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,21 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import { attributes, react as HomeContent } from "../content/home.md";
 
-export default class Home extends Component {
-  render() {
-    let { title, cats } = attributes;
-    return (
-      <>
-        <Head>
-          <title>{title}</title>
-          <link rel="icon" href="/favicon.ico" />
-          <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
-        </Head>
+export default function Home() {
+  let { title, cats } = attributes;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <main>
-          <Header title={title} />
-          <HomeContent />
-        </main>
+      <main>
+        <Header title={title} />
+        <HomeContent />
+      </main>
 
-        <Footer />
-      </>
-    );
-  }
+      <Footer />
+    </>
+  );
 }
